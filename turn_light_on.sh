e44c1c5605557e0a0eebad5a20d5a1a56e982424
Zack Matthews <zack@zackmatthews.com>
2017-08-23 22:16:20 -0400

    Run using motion -n, added time conditional so the damn thing doesnt wake me up

--- turn_light_on.sh
+++ turn_light_on.sh
@@ -3,15 +3,21 @@ export HUE_BRIDGE_IP=http://192.168.0.2/
 export BASE_API=http://192.168.0.2/api
 export USER=yDuQBo8mhfNz24YCVoCEkqeU2k05Sdos4c6E4d6D
 export CONTENT_JSON="Content-Type: application/json"
-echo "running" > ~/test.txt
 
-export IS_ENABLED=$(curl https://zackmatthews.com/motion-tracker.txt)
-echo $IS_ENABLED
-if [ $IS_ENABLED = true ]; then
+#export IS_ENABLED=$(curl https://zackmatthews.com/motion-tracker.txt)
+#echo $IS_ENABLED
+
+export TZ='America/New_York' date
+echo "Time is:"
+export CURRENT_TIME=$(date +"%H")
+
+echo $CURRENT_TIME
+echo $( date )
+
+if [[  ( $CURRENT_TIME > 0 && $CURRENT_TIME < 7 ) || $CURRENT_TIME > 18 ]]; then
 
 curl  $BASE_API/$USER/lights/1/state -H CONTENT_JSON -X PUT -d '{"on":true}' 
-curl  $BASE_API/$USER/lights/2/state -H CONTENT_JSON -X PUT -d '{"on":true}' 
 curl  $BASE_API/$USER/lights/3/state -H CONTENT_JSON -X PUT -d '{"on":true}' 
+curl  $BASE_API/$USER/lights/4/state -H CONTENT_JSON -X PUT -d '{"on":true}' 
 
 fi
-
