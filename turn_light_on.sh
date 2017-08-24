7eea545f8158150d6936a5659db46a8511ffdaa8
Zack Matthews <zack@zackmatthews.com>
2017-08-24 17:06:14 -0400

    Clearer variable names

--- turn_light_on.sh
+++ turn_light_on.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
+
+#Hue variables
 export HUE_BRIDGE_IP=http://192.168.0.2/
 export BASE_API=http://192.168.0.2/api
 export USER=yDuQBo8mhfNz24YCVoCEkqeU2k05Sdos4c6E4d6D
 export CONTENT_JSON="Content-Type: application/json"
 
-#export IS_ENABLED=$(curl https://zackmatthews.com/motion-tracker.txt)
-#echo $IS_ENABLED
-
+#Time variables
 export TZ='America/New_York' date
-echo "Time is:"
 export CURRENT_TIME=$(date +"%H")
+export MORNING=7
+export EVENING=18
 
+#Debug logging 
+echo "Time is:"
 echo $CURRENT_TIME
 echo $( date )
 
-if (( $CURRENT_TIME < 7  ||  $CURRENT_TIME > 18 )); then
+if (( $CURRENT_TIME < $MORNING  ||  $CURRENT_TIME > $EVENING )); then
+
 curl  $BASE_API/$USER/lights/1/state -H CONTENT_JSON -X PUT -d '{"on":true}' 
 curl  $BASE_API/$USER/lights/3/state -H CONTENT_JSON -X PUT -d '{"on":true}' 
 curl  $BASE_API/$USER/lights/4/state -H CONTENT_JSON -X PUT -d '{"on":true}' 
